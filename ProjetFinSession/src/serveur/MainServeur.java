90791461e824613ae0f1bf9c58a0312a51a5fb09
Frederic <comptes@techfred.ca>
2016-08-01 16:31:06 -0400

    Server, done. Send packet.

--- ProjetFinSession/src/serveur/MainServeur.java
+++ ProjetFinSession/src/serveur/MainServeur.java
@@ -6,62 +6,64 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 
+import client.thread.UDPConnectionThread;
 import serveur.data.ConfigManager;
 import serveur.data.UsersManager;
 import serveur.thread.TCPClientConnectionThread;
 
-
 public class MainServeur {
-	
+
 	private static int SERVER_PORT = 9001;
 	private static int SERVER_PORT_UDP = 9005;
-	
+
 	public static void main(String[] args) {
-		
+
 		ConfigManager.load();
 		UsersManager.load();
-		
+
 		ServerSocket serverSocket = null;
-        Socket socket = null;
+		Socket socket = null;
+		startSocketUDP();
+		try {
+			serverSocket = new ServerSocket(SERVER_PORT);
+			System.out
+					.println("Le serveur est en marche et en attente de la connexion au port " + SERVER_PORT + " ...");
 
-        try {
-			DatagramSocket SocketUDP = new DatagramSocket(SERVER_PORT_UDP);
-			System.out.println("Le serveur UDP est fonctionnel");
-		} catch (SocketException e1) {
-			e1.printStackTrace();
-		}
+			while (true) {
+				try {
+					socket = serverSocket.accept();
 
-        try {
-            serverSocket = new ServerSocket(SERVER_PORT);
-            System.out.println("Le serveur est en marche et en attente de la connexion au port "+SERVER_PORT+" ...");
-            
-			
-	        while (true) {
-	            try {
-	                socket = serverSocket.accept();
-	                
-	                System.out.println("Connexion r\u00E9ussie.");
-	    			System.out.println("Attente de l'entr\u00e9e...");
-	    			
-	                new TCPClientConnectionThread(socket).start();
-	            } catch (IOException e) {
-	                System.out.println("I/O error: " + e);
-	            }
-	        }
-	        
-	        
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally{
-        	try {
+					System.out.println("Connexion r\u00E9ussie.");
+					System.out.println("Attente de l'entr\u00e9e...");
+
+					new TCPClientConnectionThread(socket).start();
+				} catch (IOException e) {
+					System.out.println("I/O error: " + e);
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
 				serverSocket.close();
 			} catch (IOException e) {
 				System.out.println("UH?");
 			}
-        }
-		
+		}
+
 	}
-	
-	
-	
+
+	private static void startSocketUDP() {
+		DatagramSocket socket;
+		try {
+			socket = new DatagramSocket(SERVER_PORT_UDP);
+			new UDPConnectionThread(socket).start();
+			System.out.println("Le serveur UDP est fonctionnel");
+		} catch (SocketException e) {
+			e.printStackTrace();
+			System.out.println("Erreur, le thread UDP n'a pas démarré");
+		}
+	}
+
 }
--- ProjetFinSession/src/serveur/thread/GestionUserThread.java
+++ ProjetFinSession/src/serveur/thread/GestionUserThread.java
@@ -1,5 +1,45 @@
 package serveur.thread;
 
+import client.model.QuitUDP;
+import client.model.User;
+import client.utils.UtilsSendUDP;
+import serveur.model.OnlineUser;
+import serveur.model.Room;
+import serveur.model.Session;
+
 public class GestionUserThread {
 
+	// Méthode crawl user, timestamp
+	// Flush users, dans toutes les rooms. 
+	// Envoi des UDP aux clients. 
+	// Sleep
+	// Sauvegarde après le flush. 
+	// 
+	
+	private void crawlerOnlineUsers(){
+		for (OnlineUser u : Session.getInstance().getLobby().getListeUsers()){
+			if (u.getLifeBeat() + (60*1000) < System.currentTimeMillis()){
+				removeUser(u);
+				Session.getInstance().getLobby().removeUser(u);
+				// Send UDP
+			}
+		}
+	}
+	private void removeUser(OnlineUser u){
+		for (Room r : Session.getInstance().getListeRooms()){
+			r.removeUser(u);
+		}
+	}
+	private void sendUDPTimeOut(OnlineUser u, Room r){
+		QuitUDP q = new QuitUDP(u.getUid(),r.getUid(),true);
+		for (OnlineUser userToNotify : r.getListeUsers()){
+			
+		UtilsSendUDP.SendUDP(q, userToNotify.getIp(),userToNotify.getPort());
+		
+
+		}
+	}
+	private void saveData(){
+		
+	}
 }
